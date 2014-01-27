77ea9e9b6bdb00a10227bb74c56d37403c1682c9
Camille Brugel <camille.brugel@ensimag.grenoble-inp.fr>
2014-01-27 12:00:36 +0100

    to spaces

--- laurent_test.d
+++ laurent_test.d
@@ -5,19 +5,19 @@ import std.typecons;
 import std.algorithm;
 
 void main() {
-	immutable Tuple!(double, int) a = tuple(1.0,0);
-	immutable Tuple!(double, int) b = tuple(1.0,1);
-	immutable Tuple!(double, int) c = tuple(2.0,0);
-	immutable Tuple!(double, int) d = tuple(0.5,1);
+    immutable Tuple!(double, int) a = tuple(1.0,0);
+    immutable Tuple!(double, int) b = tuple(1.0,1);
+    immutable Tuple!(double, int) c = tuple(2.0,0);
+    immutable Tuple!(double, int) d = tuple(0.5,1);
 
-	immutable Tuple!(double, int)[] ab = [a, b]; 
-	immutable Tuple!(double, int)[] cd = [c, d]; 
+    immutable Tuple!(double, int)[] ab = [a, b]; 
+    immutable Tuple!(double, int)[] cd = [c, d]; 
 
     LaurentPolynomial!ab p1;
     LaurentPolynomial!cd p2;
 
     add!(p1,p2) p3;
 
-	writeln(p3.data);
+    writeln(p3.data);
 
 }
